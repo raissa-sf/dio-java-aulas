62ccb4001804069ae52c05e41dcdff29e03fc58b
raissa-sf <raissa.feitosa06@gmail.com>
2025-03-04 10:33:23 -0300

    explorando o terminal do computador

---
+++ exercicios/java-terminal/src/Calculadora.java
@@ -0,0 +1,20 @@
+/**
+ * A classe Calculadora realiza operações matemáticas simples.
+ * 
+ * @autor Raissa Feitosa
+ * @version 1.0
+ * @since 04/03/2024
+ */
+public class Calculadora {
+
+    /**
+     * Soma dois números inteiros.
+     *
+     * @param numeroUm   O primeiro número a ser somado.
+     * @param numeroDois O segundo número a ser somado.
+     * @return O resultado da soma de numeroUm e numeroDois.
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
